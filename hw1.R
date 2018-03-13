41a6282794447e75d3978ffac9ac8fb067bee4c4
Jeremy Morris <Jeremy.Morris@domo.com>
2018-03-12 22:07:00 -0600

    fix entropy calc, add expected entropy

--- hw1.R
+++ hw1.R
@@ -8,7 +8,7 @@ test <- fmt_data(readLines('Dataset/test.data'))
 train <- fmt_data(readLines('Dataset/training.data'))
 
 entropy <- function(x){
-    lcl_entropy <- -mean(x)*log(mean(x)) - mean(1-x)*log(mean(1-x))
+    lcl_entropy <- -mean(x)*log(mean(x),base=2) - mean(1-x)*log(mean(1-x),base=2)
     if( mean(x) == 1 | mean(x) == 0 ){
         lcl_entropy <- 0
     }
@@ -22,4 +22,13 @@ feature_entropy <- function(x,feature){
         entropy(y$response)
     })
     return(all_entropy)
+}
+
+expected_entropy <- function(x,feature){
+    lcl_entropy <- unlist(feature_entropy(x,feature))
+    lcl_feature <- x[ ,feature ]
+    prop_split <- split(x,f=lcl_feature)
+    prop <- unlist(lapply(prop_split,nrow)) / length(lcl_feature)
+    out <- sum(lcl_entropy * prop)
+    return(out)
 }
