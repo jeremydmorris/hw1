06cd0c91410b210762f42faefaed2d9dec6e76d4
Jeremy Morris <Jeremy.Morris@domo.com>
2018-03-12 21:39:08 -0600

    Initial commit

---
+++ hw1.R
@@ -0,0 +1,25 @@
+
+
+fmt_data <- function(x){
+    dplyr::mutate(data.frame(raw=x),response=ifelse(grepl('[+]',raw),1,0),text=gsub('[+-] ','',raw),f1=substring(text,1,1),f2=substring(text,2,2))
+}
+
+test <- fmt_data(readLines('Dataset/test.data'))
+train <- fmt_data(readLines('Dataset/training.data'))
+
+entropy <- function(x){
+    lcl_entropy <- -mean(x)*log(mean(x)) - mean(1-x)*log(mean(1-x))
+    if( mean(x) == 1 | mean(x) == 0 ){
+        lcl_entropy <- 0
+    }
+    return(lcl_entropy)
+}
+
+feature_entropy <- function(x,feature){
+    lcl_feature <- x[ , feature ]
+    local <- split(x,f=lcl_feature)
+    all_entropy <- lapply(local,function(y){
+        entropy(y$response)
+    })
+    return(all_entropy)
+}
